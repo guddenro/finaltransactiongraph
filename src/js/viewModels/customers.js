55069276658ebef2022829a5bc8589ae084df5fa
kavijain <kavijain@KAVIJAIN-IN.in.oracle.com>
2019-08-05 00:55:06 +0545

    final1

--- src/js/viewModels/customers.js
+++ src/js/viewModels/customers.js
@@ -26,20 +26,22 @@ define(['knockout', 'ojs/ojbootstrap', 'ojs/ojvalidation-base', 'ojs/ojarraydata
         var dd = String(d.getDate()).padStart(2, '0');
         var mm = String(d.getMonth() + 1).padStart(2, '0'); //January is 0!
         var yyyy = d.getFullYear();  
-       var todayDate = yyyy+"-"+mm+"-"+dd;
+        var todayDate = yyyy+"-"+mm+"-"+dd;
        //alert(todayDate);
        var mixedseries = [];
        var flag =0,flagOfSixHour =0;
        for(i = 0 ; i<data.length; i++)
        {        
+        flagOfSixHour = 0;
         mixedseries.push({name:data[i].serviceType,items:[]});
         for(j = data[i].transactions.length-1 ; j>=0 ; j--)
         {
-           
+           //console.log("j value:"+j);
            var time = data[i].transactions[j].transactionRequestDateTime;
            var givenDate = time.split("T")[0];
            var nowDate = new Date(data[i].transactions[j].transactionRequestDateTime);
            var x1 = new Date(nowDate.getTime()+now.getTimezoneOffset() * 60000);
+           //console.log("date is"+givenDate);
             if(givenDate == todayDate)
             {
               if((now.getHours()-6 )<x1.getHours())
@@ -47,20 +49,32 @@ define(['knockout', 'ojs/ojbootstrap', 'ojs/ojvalidation-base', 'ojs/ojarraydata
                 mixedseries[i]["items"].push({x:x1,y:data[i].transactions[j].count});
                 flagOfSixHour = 1;
               }
-              else{
-                  if(flagOfSixHour == 0)
-                  mixedseries[i]["items"].push({x:now,y:0});
-              }
-              flag =1;     
-
-            } 
-           else
-           {
-                if(flag ==0)
-                mixedseries[i]["items"].push({x:now,y:0});
-           }
-                    
+            }             
         }
+       
+        if(flagOfSixHour == 0)
+        {
+                    
+          var presentTime = now.getHours();
+          //console.log("present hours is:"+presentTime);
+          var pastSixHourTime = now.getHours()-6;
+          //console.log("date of today is"+todayDate);
+          for(var k = pastSixHourTime; k<=presentTime; k++){
+              //console.log("past hour data is:"+k);
+              //alert("i am k="+k);
+              var date = new Date();
+              date.setHours(k);
+              var dd = String(date.getDate()).padStart(2, '0');
+              var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+              var yyyy = date.getFullYear();  
+              var pastSixHourDate = yyyy+"-"+mm+"-"+dd;
+              //console.log("past six hour data:"+pastSixHourDate);
+              if(k>=0)
+              mixedseries[i]["items"].push({x:date.getTime(),y:0});
+          }
+                   
+        }
+        
       
        } 
        console.log(mixedseries);
